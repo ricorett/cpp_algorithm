c3cfe101d68139e7840a51d7dd0a99a2cb096093
Lana <115147586+ricorett@users.noreply.github.com>
2024-04-15 18:43:17 +0300

    append doesnt work

--- dynamic_arr/02/main.cpp
+++ dynamic_arr/02/main.cpp
@@ -1,34 +1,40 @@
 #include "main.h"
+
 int main (){
-    std::cout << "Введите фактический размер массива";
+    std::cout << "Введите фактический размер массива ";
     int actual_size;
     std::cin >> actual_size;
 
-    std::cout << "Введите логический размер массива";
+    std::cout << "Введите логический размер массива ";
     int logical_size;
     std::cin >> logical_size;
+    if (logical_size > actual_size){
+        std::cout << "Ошибка! Логический размер не может превышать фактический!" << std::endl;
+        exit(0);
+    }
 
-    int arr[actual_size];
+    int *arr = new int[logical_size];
+
     for (int i = 0; i < logical_size; i++){
         std::cout << "Введите arr[" << i << "]: ";
         std::cin >> arr[i];
     }
-    print_dynamic_array(arr, logical_size, actual_size);
 
+    print_dynamic_array(arr, logical_size, actual_size);
+    std::cout << std::endl;
     int e = 0;
     while (true){
-        std::cout << "Введите элемент для добавления в конец(Введите 0 для выхода): ";
+        std::cout << "Введите элемент для добавления в конец (Введите 0 для выхода): ";
         std::cin >> e;
         if (e == 0){
             break;
         }
-        append_to_dynamic_array(arr, logical_size, actual_size);
+        arr = append_to_dynamic_array(arr, logical_size, actual_size, e);
         print_dynamic_array(arr, logical_size, actual_size);
     }
     std::cout << "До свидания! Окончательный массив: ";
-    for (int i = 0; i < actual_size; ++i){
-        std::cout << arr[i] << " " << std::endl;
-    }
+    print_dynamic_array(arr, logical_size, actual_size);
+    delete [] arr;
     return 0;
 }
 
@@ -50,23 +56,50 @@ void print_dynamic_array(int* arr, int logical_size, int actual_size){
     }
 }
 
-int append_to_dynamic_array(int* arr, int logical_size, int actual_size);
-
-int DynamicArray::get_i(int i){
-    if (i <= last){
-        return data[i];
+int* append_to_dynamic_array(int *arr, int logical_size, int actual_size, int e){
+    int last = logical_size;
+    int *new_arr;
+    if ((last + 1) == sizeof (*arr)){
+        new_arr = new int[2 * (sizeof(*arr))];
+        for (int i = 0; i < logical_size; i++){
+            new_arr[i] = arr[i];
+        }
+        new_arr[logical_size + 1] = e;
+        ++last;
     } else {
-        throw lenght_error; //выставить стандартный эксепшн по выходы за границы
-    }
-}
-
-int DynamicArray::append(int e){
-    if (last + 1 == sizeof(*data)){
-        int new_size = 2 * (sizeof(*data));
-    }
-    for (int i = 0; i < sizeof(*data); i++){
-        new_data[i] = data[i];
-        e = new_data[last + 1];
+        for (int i = 0; i < logical_size; i++){
+            new_arr[i] = arr[i];
+        }
+        new_arr[last] = e;
         ++last;
     }
+    delete [] new_arr;
+        return new_arr;
 }
+//
+//int append_to_dynamic_array(int* arr, int logical_size, int actual_size, int e, int last){
+//    DynamicArray new_arr;
+//    *arr = new_arr.append(e, last);
+//    return *arr;
+//}
+//
+//int DynamicArray::get_i(int i, int last){
+//    if (i <= last){
+//        return array[i];
+//    } else {
+//       return OUT_OF_RANGE;
+//    }
+//}
+//
+//int DynamicArray::append(int e, int last){
+//    if (last + 1 == sizeof(*array)){
+//        new_size = 2 * (sizeof(*array));
+//    }
+//    int* new_data;
+//    for (int i = 0; i < new_size; i++){
+//        new_data[i] = array[i];
+//        ++last;
+//    }
+//    e = new_data[last + 1];
+//    return *new_data;
+//}
--- dynamic_arr/02/main.h
+++ dynamic_arr/02/main.h
@@ -1,12 +1,14 @@
 #include <iostream>
+#define OUT_OF_RANGE -1
 
-class DynamicArray {
-    int array[0];
-    last = -1;
-    int get_i(int i);
-    int append(int e);
-};
+//class DynamicArray {
+//    int* array;
+//    int new_size;
+//public:
+//    int get_i(int i, int last);
+//    int append(int e, int last);
+//};
 
 
 void print_dynamic_array(int* arr, int logical_size, int actual_size);
-int append_to_dynamic_array(int* arr, int logical_size, int actual_size);
+int* append_to_dynamic_array(int* arr, int logical_size, int actual_size, int e);
